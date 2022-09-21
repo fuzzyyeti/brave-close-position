777444c46d28da10352eeec98a374dcade39e2b1
Fuzzy Yeti <fuzzyyeti@github.com>
2022-09-21 01:29:23 -0600

    Buffer plug-in

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,21 @@
 /* config-overrides.js */
 const webpack = require('webpack');
 module.exports = function override(config, env) {
+
     config.resolve.fallback = {
         crypto: false,
         stream: false,
     };
+    config.resolve.alias = {
+            process: 'process/browser',
+            stream: "stream-browserify",
+            zlib: "browserify-zlib"
+    };
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    );
     return config;
 }
